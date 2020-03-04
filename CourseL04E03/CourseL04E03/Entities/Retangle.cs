332302c6b7b9e3440a453fd4ecab54bb11f6c0e4
Sergio Martins <sergio.cmartins@gmail.com>
2020-03-04 12:51:00 -0300

    Lesson 04 Exercise 03 - Various Retangle Measurements

---
+++ CourseL04E03/CourseL04E03/Entities/Retangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CourseL04E03.Entities
+{
+    class Retangle
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+
+        public double Area()
+        {
+            return Height * Width;
+        }
+
+        public double Perimeter()
+        {
+            return (Height * 2) + (Width * 2);
+        }
+
+        public double Diagonal()
+        {
+            return Math.Sqrt((Height * Height) + (Width * Width));
+        }
+    }
+}
---
+++ CourseL04E03/CourseL04E03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using CourseL04E03.Entities;
+
+namespace CourseL04E03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangle R = new Retangle();
+
+            Console.WriteLine("Enter the Height and Width of the retangle");
+            R.Height = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            R.Width = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Area: " + R.Area().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimeter: " + R.Perimeter().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal: " + R.Diagonal().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
