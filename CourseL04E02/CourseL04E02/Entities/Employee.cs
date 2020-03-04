2411cfd00fd08d96dd967c1765d4eba54d403253
Sergio Martins <sergio.cmartins@gmail.com>
2020-03-04 12:50:16 -0300

    Lesson 04 Exercise 02 - Average Salary of two Employees

---
+++ CourseL04E02/CourseL04E02/Entities/Employee.cs
@@ -0,0 +1,8 @@
+﻿namespace CourseL04E02.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+    }
+}
---
+++ CourseL04E02/CourseL04E02/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using CourseL04E02.Entities;
+
+namespace CourseL04E02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee();
+            Employee e2 = new Employee();
+
+            Console.WriteLine("First employee data: ");
+            Console.Write("Name: ");
+            e1.Name = Console.ReadLine();
+            Console.Write("Salary: ");
+            e1.Salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Second employee data: ");
+            Console.Write("Name: ");
+            e2.Name = Console.ReadLine();
+            Console.Write("Salary: ");
+            e2.Salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double avgSalary = (e1.Salary + e2.Salary) / 2.0;
+            Console.WriteLine("Average Salary: " + avgSalary.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
