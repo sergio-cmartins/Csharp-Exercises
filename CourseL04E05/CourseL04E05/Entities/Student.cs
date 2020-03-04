7d808f4b9c2977761233b0b4c06ae443bae2f35e
Sergio Martins <sergio.cmartins@gmail.com>
2020-03-04 12:53:01 -0300

    Lesson 04 Exercise 05 - Simple Students Grading Evaluation

---
+++ CourseL04E05/CourseL04E05/Entities/Student.cs
@@ -0,0 +1,39 @@
+﻿namespace CourseL04E05.Entities
+{
+    class Student
+    {
+        public string Name { get; set; }
+        public double Grade01 { get; set; }
+        public double Grade02 { get; set; }
+        public double Grade03 { get; set; }
+
+        public double FinalGrade()
+        {
+            return Grade01 + Grade02 + Grade03;
+        }
+
+        public bool Approved()
+        {
+            if (FinalGrade() >= 60.0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        public double MissingGrade()
+        {
+            if (Approved() == true)
+            {
+                return 0.0;
+            }
+            else
+            {
+                return 60.0 - FinalGrade();
+            }
+        }
+    }
+}
---
+++ CourseL04E05/CourseL04E05/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using CourseL04E05.Entities;
+
+namespace CourseL04E05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+
+            Console.Write("Student Name: ");
+            student.Name = Console.ReadLine();
+            Console.WriteLine("Type de Studend three grades: ");
+            student.Grade01 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            student.Grade02 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            student.Grade03 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Final Grade: " + student.FinalGrade().ToString("F2", CultureInfo.InvariantCulture));
+            if (student.Approved())
+            {
+                Console.WriteLine("APPROVED");
+            }
+            else
+            {
+                Console.WriteLine("DISAPPROVED");
+                Console.WriteLine("MISSING " 
+                                + student.MissingGrade().ToString("F2", CultureInfo.InvariantCulture) 
+                                + " POINTS");
+            }
+        }
+    }
+}
