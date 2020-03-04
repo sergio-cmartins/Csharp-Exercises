c345c440cc0731791c3aa9dc8e84a39e7d3df01b
Sergio Martins <sergio.cmartins@gmail.com>
2020-03-04 12:49:40 -0300

    Lesson 04 Exercise 01 - Check older Person

---
+++ CourseL04E01/CourseL04E01/Entities/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace CourseL04E01.Entities
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ CourseL04E01/CourseL04E01/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using CourseL04E01.Entities;
+
+namespace CourseL04E01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person();
+            Person p2 = new Person();
+
+            Console.WriteLine("First Person Data:");
+            Console.Write("Name: ");
+            p1.Name = Console.ReadLine();
+            Console.Write("Age: ");
+            p1.Age = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Second Person Data:");
+            Console.Write("Name: ");
+            p2.Name = Console.ReadLine();
+            Console.Write("Age: ");
+            p2.Age = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Older person: ");
+            if (p1.Age > p2.Age)
+            {
+                Console.WriteLine(p1.Name);
+            }
+            else
+            {
+                Console.WriteLine(p2.Name);
+            }
+        }
+    }
+}
