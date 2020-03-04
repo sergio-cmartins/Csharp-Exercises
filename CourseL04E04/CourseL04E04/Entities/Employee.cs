635259684fbc63d64cd99cc7e9190b077437478d
Sergio Martins <sergio.cmartins@gmail.com>
2020-03-04 12:52:04 -0300

    Lesson 04 Exercise 04 - Simple Employee Salary Calcs

---
+++ CourseL04E04/CourseL04E04/Entities/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace CourseL04E04.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public double GrossPay { get; set; }
+        public double Tax { get; set; }
+
+        public double NetPay()
+        {
+            return GrossPay - Tax;
+        }
+
+        public void RaiseSalary(double percentage)
+        {
+            GrossPay *= (1 + (percentage / 100.0));
+        }
+
+        public override string ToString()
+        {
+            return Name
+                + ", $ "
+                + NetPay().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ CourseL04E04/CourseL04E04/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using CourseL04E04.Entities;
+
+namespace CourseL04E04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            Console.Write("Name: ");
+            employee.Name = Console.ReadLine();
+            Console.Write("Gross Pay: ");
+            employee.GrossPay = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Tax: ");
+            employee.Tax = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("\nEmployee: " + employee);
+            Console.Write("\nType the percentare to raise the salary: ");
+            employee.RaiseSalary(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine("\nUpdated Data: " + employee);
+
+        }
+    }
+}
