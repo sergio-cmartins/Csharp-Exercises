b490dc553e4d59befc277a2c791247a616845dfd
Sergio Martins <sergio.cmartins@gmail.com>
2020-03-05 10:57:40 -0300

    Lesson 05 Exercise 01 - Bank Account - (Encapsulation and overload)

---
+++ CourseL05E01/BankAccount/Entities/BankAccount.cs
@@ -0,0 +1,44 @@
+﻿using System.Globalization;
+
+namespace BankAccount.Entities
+{
+    class Account
+    {
+        private double withdrawalTax = 5.00;
+
+        public int AccNumber { get; private set; }
+        public string OwnerName { get; set; }
+        public double Balance { get; private set; }
+
+        public Account(int accNumber, string ownerName)
+        {
+            AccNumber = accNumber;
+            OwnerName = ownerName;
+        }
+
+        public Account(int accNumber, string ownerName, double initialDeposit) : this(accNumber, ownerName)
+        {
+            Deposit(initialDeposit);
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance -= (amount + withdrawalTax);
+        }
+
+        public override string ToString()
+        {
+            return "Account "
+                 + AccNumber
+                 + ", Account Owner: "
+                 + OwnerName
+                 + ", Balance: $ "
+                 + Balance.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ CourseL05E01/BankAccount/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using BankAccount.Entities;
+
+namespace BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account;
+
+            Console.Write("Enter the account number: ");
+            int accNumber = int.Parse(Console.ReadLine());
+            Console.Write("Enter the Account Owner: ");
+            string accOwner = Console.ReadLine();
+            Console.Write("Make Initial Deposit (y/n)? ");
+            char response = char.Parse(Console.ReadLine());
+            if (response == 'y' || response == 'Y')
+            {
+                Console.Write("Enter the Deposit Amount: ");
+                double initialDeposit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account = new Account(accNumber, accOwner, initialDeposit);
+            }
+            else
+            {
+                account = new Account(accNumber, accOwner);
+            }
+
+            Console.WriteLine("\nAccount data:");
+            Console.WriteLine(account);
+
+            Console.Write("\nEnter a deposit amount: ");
+            account.Deposit(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine("Uppdated account data:");
+            Console.WriteLine(account);
+
+            Console.Write("\nEnter a withdrawal amount: ");
+            account.Withdraw(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            Console.WriteLine("Uppdated account data:");
+            Console.WriteLine(account);
+
+        }
+    }
+}
