9fe07d3355f00f740af663a9cccf7075e26ec312
Sergio Martins <sergio.cmartins@gmail.com>
2020-03-19 12:02:11 -0300

    Lesson 10 Exercise 02 - Products Tags (Inheritance and Polymorphism)

---
+++ CourseL10E02/ProductTags/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using ProductTags.Entities;
+
+namespace ProductTags
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Product #{i} data:");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                char pType = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                if (pType == 'i' || pType == 'I')
+                {
+                    Console.Write("Customs fee: ");
+                    double customsFee = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    products.Add(new ImportedProduct(name, price, customsFee));
+                }
+                else if (pType == 'u' || pType == 'U')
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime manufactureDate = DateTime.Parse(Console.ReadLine());
+                    products.Add(new UsedProduct(name, price, manufactureDate));
+
+                }
+                else
+                {
+                    products.Add(new Product(name, price));
+                }
+            }
+
+            Console.WriteLine("\nPRICE TAGS:");
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.PriceTag());
+            }
+        }
+    }
+}
