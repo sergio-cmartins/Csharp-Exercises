fb17cd54570019968384e998dc4e98f620c1830e
Sergio Martins <sergio.cmartins@gmail.com>
2020-03-28 11:32:33 -0300

    Lesson 11 Exercise 01 - Bank Account (Exception Handling)

---
+++ CourseL11E01/BankAccount/Entities/Account.cs
@@ -0,0 +1,42 @@
+﻿using BankAccount.Entities.Exceptions;
+
+namespace BankAccount.Entities
+{
+    class Account
+    {
+        public int Number { get; private set; }
+        public string Holder { get; set; }
+        public double Balance { get; protected set; }
+        public double WithdrawLimit { get; set; }
+
+        public Account(int number, string holder, double balance, double withdrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            WithdrawLimit = withdrawLimit;
+            Balance = 0.0;
+
+            //Adds the balance as initial Deposit
+            Deposit(balance);
+
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > WithdrawLimit)
+            {
+                throw new DomainException("The amount exceeds withdraw limit.");
+            }
+            if (amount > Balance)
+            {
+                throw new DomainException("Not enough balance.");
+            }
+            Balance -= amount;
+        }
+    }
+}
---
+++ CourseL11E01/BankAccount/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using BankAccount.Entities;
+using BankAccount.Entities.Exceptions;
+
+namespace BankAccount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter account data");
+                Console.Write("Number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial balance: ");
+                double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Withdraw limit: ");
+                double withdrawLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Account account = new Account(number, holder, balance, withdrawLimit);
+
+                Console.Write("\nEnter amount for withdraw: ");
+                account.Withdraw(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+                Console.WriteLine("New Balance: " + account.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Incorrect format for field.");
+            }
+        }
+    }
+}
