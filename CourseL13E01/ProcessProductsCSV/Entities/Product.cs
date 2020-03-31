bba86b6c67c6400b15953d2be40170b5857f9e71
Sergio Martins <sergio.cmartins@gmail.com>
2020-03-31 11:15:17 -0300

    Lesson 13 Exercise 01 - Read and Summarise product Files - file manipulation, stream reading and stream writing

---
+++ CourseL13E01/ProcessProductsCSV/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProcessProductsCSV.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public double Total
+        {
+            get { return Price * Quantity; }
+        }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+    }
+}
---
+++ CourseL13E01/ProcessProductsCSV/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Globalization;
+using ProcessProductsCSV.Entities;
+
+namespace ProcessProductsCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please inform the source file with full path: ");
+            string sourceFile = Console.ReadLine();
+
+            try
+            {
+                string outpath = Path.GetDirectoryName(sourceFile) + Path.DirectorySeparatorChar + "out";
+                string outfile = outpath + Path.DirectorySeparatorChar + "summary.csv";
+
+                Directory.CreateDirectory(outpath);
+
+                using (StreamReader sr = File.OpenText(sourceFile))
+                {
+                    using (StreamWriter sw = File.CreateText(outfile))
+                    {
+                        while (!sr.EndOfStream){
+                            string[] fields = sr.ReadLine().Split(',');
+                            Product p = new Product(fields[0], double.Parse(fields[1], CultureInfo.InvariantCulture), int.Parse(fields[2]));
+                            sw.WriteLine(p.Name + "," + p.Total.ToString("F2", CultureInfo.InvariantCulture));
+                        }
+                    }
+                }
+
+            } catch (IOException e)
+            {
+                Console.WriteLine("An error occured during execution:");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
