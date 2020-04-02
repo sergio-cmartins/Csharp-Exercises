669c710e1b25966b122003b09af982a64f41784d
Sergio Martins <sergio.cmartins@gmail.com>
2020-04-02 11:38:22 -0300

    Lesson 14 Exercise 01 - Car Rental (bad solution, without interface)

---
+++ CourseL14E01/CarRentalV1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using CarRentalV1.Entities;
+using CarRentalV1.Services;
+namespace CarRentalV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter rental data");
+            Console.Write("Car model: ");
+            string model = Console.ReadLine();
+            Console.Write("Pickup (dd/MM/yyyy hh:mm): ");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.Write("Return (dd/MM/yyyy hh:mm): ");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+            //Rental service calling
+            Console.Write("Enter price per hour: ");
+            double pricePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Enter price per day: ");
+            double pricePerDay = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            RentalService rentalService = new RentalService(pricePerHour, pricePerDay);
+            rentalService.ProcessInvoice(carRental);
+
+            //Output after processing invoice:
+            Console.WriteLine("\nINVOICE:");
+            Console.WriteLine(carRental.Invoice);
+
+
+        }
+    }
+}
---
+++ CourseL14E01/CarRentalV1/Services/RentalService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CarRentalV1.Entities;
+
+namespace CarRentalV1.Services
+{
+    class RentalService
+    {
+        public double PricePerHour { get; private set; }
+        public double PricePerDay { get; private set; }
+
+        //Not the best practice for such service, used for didatic purposes.
+        private BrazilTaxService brazilTaxService = new BrazilTaxService();
+
+        public RentalService(double pricePerHour, double pricePerDay)
+        {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+        }
+
+        public void ProcessInvoice(CarRental carRental)
+        {
+            TimeSpan rentalTime = carRental.Finish.Subtract(carRental.Start);
+
+            double basicPayment = (rentalTime.TotalHours <= 12.0) ? Math.Ceiling(rentalTime.TotalHours) * PricePerHour : Math.Ceiling(rentalTime.TotalDays) * PricePerDay;
+            double tax = brazilTaxService.Tax(basicPayment);
+
+            carRental.Invoice = new Invoice(basicPayment, tax);
+        }
+    }
+}
