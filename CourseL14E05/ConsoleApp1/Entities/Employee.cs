d0287313e85c6353a8932b1fb7c1d20346d539d5
Sergio Martins <sergio.cmartins@gmail.com>
2020-04-06 11:41:12 -0300

    Lesson 14 Exercise 05 - Implementing Icomparable interface on an Object

---
+++ CourseL14E05/ConsoleApp1/Entities/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1.Entities
+{
+    class Employee : IComparable
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string name, double salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+
+        public override string ToString()
+        {
+            return Name
+                + ", " +
+                Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Employee))
+            {
+                throw new ArgumentException("Comparing Error: argument is not an Employee object");
+            }
+            Employee other = obj as Employee;
+            if (Salary.CompareTo(other.Salary) !=0)
+            {
+                return Salary.CompareTo(other.Salary);
+            }
+            else
+            {
+                return Name.CompareTo(other.Name);
+            }
+            
+        }
+    }
+}
---
+++ CourseL14E05/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Globalization;
+using ConsoleApp1.Entities;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string file = @"E:\trf\employees.csv";
+
+            try
+            {
+                using( StreamReader sr = File.OpenText(file))
+                {
+                    List<Employee> employees = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(',');
+                        string name = line[0];
+                        double salary = double.Parse(line[1], CultureInfo.InvariantCulture);
+                        employees.Add(new Employee(name, salary));
+                    }
+
+                    employees.Sort();
+                    foreach (Employee employee in employees)
+                    {
+                        Console.WriteLine(employee);
+                    }
+
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error has ocurred:");
+                Console.WriteLine(e.Message);
+            }
+            
+        }
+    }
+}
