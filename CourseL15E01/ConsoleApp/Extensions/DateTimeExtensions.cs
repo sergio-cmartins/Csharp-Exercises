063c780188b13e84ed87b14bb2f5f1eefe1c260b
Sergio Martins <sergio.cmartins@gmail.com>
2020-04-07 11:16:03 -0300

    Lesson 15 Exercise 01 - Implementing Extension Methods

---
+++ CourseL15E01/ConsoleApp/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime thisObj)
+        {
+            TimeSpan ts = DateTime.Now.Subtract(thisObj);
+            if (ts.TotalSeconds < 60)
+            {
+                return ts.TotalSeconds.ToString("F1", CultureInfo.InvariantCulture) + " second(s)";
+            }
+            else if (ts.TotalMinutes < 60)
+            {
+                return ts.TotalMinutes.ToString("F1", CultureInfo.InvariantCulture) + " minute(s)";
+            }
+            else if (ts.TotalHours < 24)
+            {
+                return ts.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " hour(s)";
+
+            }
+            else
+            {
+                return ts.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " day(s)"; ;
+            }
+            
+        }
+    }
+}
---
+++ CourseL15E01/ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ConsoleApp.Extensions;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type a valid date to calculate the elapsed time (type an invalid date to end the program)");
+            try
+            {
+                while (true)
+                {
+                    Console.Write("Type a valid date => ");
+                    DateTime dt = DateTime.Parse(Console.ReadLine());
+                    Console.WriteLine("Date Typed: " + dt);
+                    Console.WriteLine("Elapsed Time: " + dt.ElapsedTime());
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("\nProcesso Concluido!");
+            }
+        }
+    }
+}
