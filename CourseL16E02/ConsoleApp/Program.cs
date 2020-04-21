3a4db3de16412e1421c1f56872b1070d1c73687f
Sergio Martins <sergio.cmartins@gmail.com>
2020-04-21 11:16:12 -0300

    Lesson 16 Exercise 02 - Basic Ling Exercise

---
+++ CourseL16E02/ConsoleApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Define the Data Source
+            //using a simple file to list but can be any data source
+            List<string> dsList = File.ReadAllLines(@"E:\trf\stringList.txt").ToList<string>();
+
+            //Define the Query expression
+            // In this example I'm searching(where) all names begining with D and converting them to uppercase(select).
+            var result = dsList
+                .Where(p => p.Substring(0, 1) == "D")
+                .Select(r => r.ToUpper())
+                .ToList();
+
+            //Console.WriteLine("Hello World!");
+            result.ForEach(p => { Console.WriteLine(p); });
+        }
+    }
+}
